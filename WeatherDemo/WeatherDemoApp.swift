81451ed4b69415f41793dc11c2cf51c5fe260659
Gustavo <gustavolirasn@gmail.com>
2022-06-16 12:21:23 -0300

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Gustavo Santos on 16/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
